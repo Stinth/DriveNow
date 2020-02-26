06578df9a2f19a7d96e2aafa2f3dae51cc43a02a
Stinth <malthejelstrup@gmail.com>
2020-02-26 01:54:23 +0100

    Attempts to fixMultiTravel

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import os
+
 ###############################################
 # Place DriveNow data in the path given below #
 ###############################################
@@ -10,7 +11,7 @@ removeList = [[" ", "_"], ["(", ""], [")", ""]]
 for remove, replaceWith in removeList:
     df.columns = [c.replace(remove, replaceWith) for c in df.columns]
 # 'coerce' results in NaN for entries that can't be converted
-df.Batteristatus_start = pd.to_numeric(df.Batteristatus_start,errors='coerce')
+df.Batteristatus_start = pd.to_numeric(df.Batteristatus_start, errors='coerce')
 df.Batteristatus_slut = pd.to_numeric(df.Batteristatus_slut, errors='coerce')
 
 
@@ -30,7 +31,7 @@ df.drop(df.tail(6).index, inplace=True)
 df.drop(df[df.BilID == "-"].index, inplace=True)
 # replace "," with "." in strings
 for column in ["Latitude_Start", "Latitude_Slut", "Longitude_Start", "Longitude_Slut"]:
-    df[column] = df[column].str.replace(",",".").astype(float)
+    df[column] = df[column].str.replace(",", ".").astype(float)
 
 
 # function should only be used on df's filtered to only ONE BilID
@@ -54,18 +55,49 @@ def fixDataMissing(df):
         for idx in df[(df.ToZoneID == 0) | (df.FromZoneID == 0) & (df.Latitude_Slut != "0")].index.values.astype(int):
             df.at[idx, "ToZoneID"] = 999999
 
-        # If PersonID = PersonID.shift(periods=-1) and time between slut and start < 5 min
-            # use first coord start and last coord slut, first time start last time slut etc. combine km kørt
+        # If PersonID = PersonID.shift(periods=1) and time between slut and start < 5 min
+        # use first coord start and last coord slut, first time start last time slut etc. combine km kørt
+    return df
+
+
+# Column tables for fixing
+mergeColumns = ["Latitude_Start", "Longitude_Start", "Reservationstidspunkt", "Start_tidspunkt", "Batteristatus_start",
+                "Km_kørt", "FromZoneID", "tripDuration"]
+
+
+def fixMultiTravel(df):
+    if len(df.BilID.unique()) != 1:
+        print("Please only use function fixDataMissing on df's filtered to ONE BilID!")
+    else:
+        alterRows = df[(df.PersonID == df.PersonID.shift(periods=1)) & (df.idleTime <= 6)]
+        dfshift = df.shift(periods=1)[(df.PersonID == df.PersonID.shift(periods=1)) & (df.idleTime <= 6)]
+        dfnoshift = df[(df.PersonID == df.PersonID.shift(periods=1)) & (df.idleTime <= 6)]
+        timeSeries = dfnoshift.tripDuration + dfshift.tripDuration - dfnoshift.idleTime.astype(float)
+        df.loc[(df.PersonID == df.PersonID.shift(periods=1)) & (df.idleTime <= 6)].assign(
+            Latitude_Start=dfshift.Latitude_Start,
+            Longitude_Start=dfshift.Longitude_Start,
+            Reservationstidspunkt=dfshift.Reservationstidspunkt,
+            Start_tidspunkt=dfshift.Start_tidspunkt,
+            Batteristatus_start=dfshift.Batteristatus_start,
+            Km_kørt=dfnoshift.Km_kørt + dfshift.Km_kørt,
+            FromZoneID=dfshift.FromZoneID,
+            tripDuration=dfnoshift.tripDuration + dfshift.tripDuration - dfnoshift.idleTime.astype(float)
+        )
 
 
 # Adds column tripDuration in minutes to the dataframe
 df["tripDuration"] = (df.Slut_tidspunkt - df.Start_tidspunkt).astype("timedelta64[m]")
 # Adds column idleTime in minutes to the dataframe
 df["idleTime"] = "default"
+
 for BilID in df.BilID.unique():
+    df[df.BilID == BilID] = fixDataMissing(df[df.BilID == BilID])
+
+    #
+
+    # Populates column idleTime by difference in last use end minus this use start time
     df.loc[df["BilID"] == BilID, ["idleTime"]] = (df[df["BilID"] == BilID]["Start_tidspunkt"] - df[df["BilID"] == BilID]
     ["Slut_tidspunkt"].shift(periods=1)).astype("timedelta64[m]")
-
 
 import plotly.express as px
 
