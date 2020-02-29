fc5ca73e75708ef4462080e8545b0d794d9a04d3
Stinth <malthejelstrup@gmail.com>
2020-02-29 04:27:38 +0100

    Finished fixMissingData Small change still needed for fixMultiTravel Vectorized both functions instead of having them loop through data

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -24,9 +24,11 @@ def stringToDatetime(columns):
 
 
 stringToDatetime(["Reservationstidspunkt", "Start_tidspunkt", "Slut_tidspunkt"])
-df.sort_values(by=["Start_tidspunkt"], inplace=True)
+# Sorts df by BilID then Start_tidspunkt, this ensures all rides are in order and we can reference rows above as last trip
+df.sort_values(by=["BilID", "Start_tidspunkt"], inplace=True)
 # Dropping last 6 rows as the date for these rides are 2 months later than the last datapoint in the rest of the data
-df.drop(df.tail(6).index, inplace=True)
+# We sort only by Start_tidspunkt, not inplace, to get the true 6 last rides regardless of BilID
+df.drop(df.sort_values(by=["Start_tidspunkt"]).tail(6).index, inplace=True)
 # Dropping data lines with "-" as BilID
 df.drop(df[df.BilID == "-"].index, inplace=True)
 # replace "," with "." in strings
@@ -36,62 +38,88 @@ for column in ["Latitude_Start", "Latitude_Slut", "Longitude_Start", "Longitude_
 
 # function should only be used on df's filtered to only ONE BilID
 def fixDataMissing(df):
-    if len(df.BilID.unique()) != 1:
-        print("Please only use function fixDataMissing on df's filtered to ONE BilID!")
+    if len(df.BilID.unique()) == 1:
+        print("Please provide full df and BilID as second variable!")
     else:
         # Drop all rows that start and stop in same exact X-coordinate
         df.drop(df[(df.Latitude_Start == df.shift(periods=-1).Latitude_Start) & (
                 df.Latitude_Start == df.Latitude_Slut)].index, inplace=True)
 
         # If end point data is missing, fill in start data from next trip.
-        for idx in df[(df.ToZoneID == 0) & (df.Latitude_Slut == "0") & (
-                df.Latitude_Start != df.Latitude_Start.shift(periods=-1))].index.values.astype(int):
-            df.at[idx, "Latitude_Slut"] = df.shift(periods=-1).at[idx, "Latitude_Start"]
-            df.at[idx, "Longitude_Slut"] = df.shift(periods=-1).at[idx, "Longitude_Start"]
-            df.at[idx, "ToZoneID"] = df.shift(periods=-1).at[idx, "ToZoneID"]
+        # for idx in df[(df.BilID == BilID) & (df.ToZoneID == 0) & (df.Latitude_Slut == "0") & (
+        #         df.Latitude_Start != df.Latitude_Start.shift(periods=-1))].index.values.astype(int):
+        #     df.at[idx, "Latitude_Slut"] = df.shift(periods=-1).at[idx, "Latitude_Start"]
+        #     df.at[idx, "Longitude_Slut"] = df.shift(periods=-1).at[idx, "Longitude_Start"]
+        #     df.at[idx, "ToZoneID"] = df.shift(periods=-1).at[idx, "ToZoneID"]
+        dfshift = df.shift(periods=-1)[
+            (df.ToZoneID == 0) & (df.Latitude_Slut == 0) & (df.Latitude_Start != df.Latitude_Start.shift(periods=-1))]
+        newValues = pd.DataFrame(data={
+            "Latitude_Slut": dfshift.Latitude_Start,
+            "Longitude_Slut": dfshift.Longitude_Start,
+            "ToZoneID": dfshift.FromZoneID
+        })
+        df.loc[(df.ToZoneID == 0) & (df.Latitude_Slut == 0) & (df.Latitude_Start != df.Latitude_Start.shift(periods=-1)),
+               ["Latitude_Slut", "Longitude_Slut", "ToZoneID"]] = newValues
 
-        # If FromZoneID or ToZoneID is empty, but coords populated, set *ZoneID=999999
+        # If FromZoneID or ToZoneID is empty, but coords are populated, set *ZoneID=999999
         # This is a bug caused by cars parked on bridges or near water.
-        for idx in df[(df.ToZoneID == 0) | (df.FromZoneID == 0) & (df.Latitude_Slut != "0")].index.values.astype(int):
-            df.at[idx, "ToZoneID"] = 999999
-
-        # If PersonID = PersonID.shift(periods=1) and time between slut and start < 5 min
-        # use first coord start and last coord slut, first time start last time slut etc. combine km kørt
+        # for idx in df[(df.BilID == BilID) &
+        #         (df.ToZoneID == 0) & (df.Latitude_Slut != 0)].index.values.astype(int):
+        #     df.at[idx, "ToZoneID"] = 999999
+        df.loc[(df.ToZoneID == 0) & (df.Latitude_Slut != 0), ["ToZoneID"]] = 999999
+        df.loc[(df.FromZoneID == 0) & (df.Latitude_Start != 0), ["FromZoneID"]] = 999999
     return df
 
 
-# Column tables for fixing
+# Column tables for fixing multi travel by same PersonID with no delay between
 mergeColumns = ["Latitude_Start", "Longitude_Start", "Reservationstidspunkt", "Start_tidspunkt", "Batteristatus_start",
                 "Km_kørt", "FromZoneID", "tripDuration"]
+# Threshold deciding cutoff for merging trips into one
+threshold = 6
 
 
 def fixMultiTravel(df):
-    if len(df.BilID.unique()) != 1:
-        print("Please only use function fixDataMissing on df's filtered to ONE BilID!")
+    if len(df.BilID.unique()) == 1:
+        print("Please provide full df and BilID as second variable!")
     else:
-        alterRows = df[(df.PersonID == df.PersonID.shift(periods=1)) & (df.idleTime <= 6)]
-        dfshift = df.shift(periods=1)[(df.PersonID == df.PersonID.shift(periods=1)) & (df.idleTime <= 6)]
-        dfnoshift = df[(df.PersonID == df.PersonID.shift(periods=1)) & (df.idleTime <= 6)]
-        timeSeries = dfnoshift.tripDuration + dfshift.tripDuration - dfnoshift.idleTime.astype(float)
-        df.loc[(df.PersonID == df.PersonID.shift(periods=1)) & (df.idleTime <= 6)].assign(
-            Latitude_Start=dfshift.Latitude_Start,
-            Longitude_Start=dfshift.Longitude_Start,
-            Reservationstidspunkt=dfshift.Reservationstidspunkt,
-            Start_tidspunkt=dfshift.Start_tidspunkt,
-            Batteristatus_start=dfshift.Batteristatus_start,
-            Km_kørt=dfnoshift.Km_kørt + dfshift.Km_kørt,
-            FromZoneID=dfshift.FromZoneID,
-            tripDuration=dfnoshift.tripDuration + dfshift.tripDuration - dfnoshift.idleTime.astype(float)
-        )
+        while len(df.loc[(df.PersonID == df.PersonID.shift(periods=1)) & (df.PersonID != df.PersonID.shift(periods=2))
+                         & (df.idleTime <= threshold)]) != 0:
+            dfshift = df.shift(periods=1)[(df.PersonID == df.PersonID.shift(periods=1)) & (
+                    df.PersonID != df.PersonID.shift(periods=2)) & (df.idleTime <= threshold)]
+            dfnoshift = df[(df.PersonID == df.PersonID.shift(periods=1)) & (df.PersonID != df.PersonID.shift(periods=2))
+                           & (df.idleTime <= threshold)]
+            mergeddf = pd.DataFrame(data={
+                "Latitude_Start": dfshift.Latitude_Start,
+                "Longitude_Start": dfshift.Longitude_Start,
+                "Reservationstidspunkt": dfshift.Reservationstidspunkt,
+                "Start_tidspunkt": dfshift.Start_tidspunkt,
+                "Batteristatus_start": dfshift.Batteristatus_start,
+                "Km_kørt": dfnoshift.Km_kørt + dfshift.Km_kørt,
+                "FromZoneID": dfshift.FromZoneID,
+                "tripDuration": dfnoshift.tripDuration + dfshift.tripDuration - dfnoshift.idleTime.astype(float)
+            })
+            shiftIndex = df.shift(periods=-1)
+            removeIndex = shiftIndex.loc[(shiftIndex.PersonID == shiftIndex.PersonID.shift(periods=1)) & (
+                    shiftIndex.PersonID != shiftIndex.PersonID.shift(periods=2)) & (
+                                                 shiftIndex.idleTime <= threshold)].index
+            # Checks if trip above is driven by same PersonID and with idleTime <= 6 minutes
+            # If true merge rows and delete above row
+            df.loc[(df.PersonID == df.PersonID.shift(periods=1)) & (df.PersonID != df.PersonID.shift(periods=2)) & (
+                    df.idleTime <= threshold)] = mergeddf
+            df.drop(removeIndex, inplace=True)
+            print(len(df))
+            # TODO redo this to alter top rows slut results, that way no nans will come maybe
 
 
 # Adds column tripDuration in minutes to the dataframe
 df["tripDuration"] = (df.Slut_tidspunkt - df.Start_tidspunkt).astype("timedelta64[m]")
 # Adds column idleTime in minutes to the dataframe
 df["idleTime"] = "default"
+# fixing various data problems
+df = fixDataMissing(df)
 
 for BilID in df.BilID.unique():
-    df[df.BilID == BilID] = fixDataMissing(df[df.BilID == BilID])
+    # df[df.BilID == BilID] = fixDataMissing(df, BilID)
 
     #
 
@@ -99,6 +127,8 @@ for BilID in df.BilID.unique():
     df.loc[df["BilID"] == BilID, ["idleTime"]] = (df[df["BilID"] == BilID]["Start_tidspunkt"] - df[df["BilID"] == BilID]
     ["Slut_tidspunkt"].shift(periods=1)).astype("timedelta64[m]")
 
+# merging trips within a short timespan by the same PersonID
+fixMultiTravel(df)
 import plotly.express as px
 
 # fig = px.scatter_mapbox(df.tail(100), lat="Latitude_Start", lon="Longitude_Start", hover_name="TurID", hover_data=["BilID","tripDuration"],
