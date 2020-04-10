2b1cf6c7fc6c14a3a8fdc4126cee334355249ed6
Stinth <malthejelstrup@gmail.com>
2020-04-10 20:47:17 +0200

    Expanded the main script greatly

--- MainScript.py
+++ MainScript.py
@@ -4,36 +4,53 @@ import os
 from dateutil import tz
 from datetime import datetime, timedelta
 import seaborn
+import matplotlib.pyplot as plt
 
 from DataAnalysis import *
 from PlotMap import *
 
-###############################################
-# Place DriveNow data in the path given below #
-###############################################
+# Dataframe visual settings
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
 
-df = pd.read_csv("Data/DTU-data-til-case_LTMZones1.csv", sep=";")
 
-#####################
-# Dataframe Cleanup #
-initial_columnname_changes(df)
-stringToDatetime(df, ["Reservationstidspunkt", "Start_tidspunkt", "Slut_tidspunkt"])
-sort_drop_nans_add_colums(df)
+try:
+    df = pd.read_pickle("Data/DTU_DriveNow_Cleaned_Data")
+except FileNotFoundError:
+    ###############################################
+    # Place DriveNow data in the path given below #
+    ###############################################
 
-################
-# Data cleanup #
-# fixing various data problems
-fixDataMissing(df)
-update_idleTime(df)
-# merging trips within a short timespan by the same PersonID
-fixMultiTravel(df)
-update_idleTime(df)
+    df = pd.read_csv("Data/DTU-data-til-case_LTMZones1.csv", sep=";")
 
-####################
-# Outlier handling #
-# drops any rows with a tripDuration larger than 3 times interquantile range
-OutlierHandling(df)
+    #####################
+    # Dataframe Cleanup #
+    initial_columnname_changes(df)
+    stringToDatetime(df, ["Reservationstidspunkt", "Start_tidspunkt", "Slut_tidspunkt"])
+    sort_drop_nans_add_colums(df)
+    add_hour_and_date(df)
+
+    ################
+    # Data cleanup #
+    # fixing various data problems
+    fixDataMissing(df)
+    update_idleTime(df)
+    # merging trips within a short timespan by the same PersonID
+    fixMultiTravel(df)
+    update_idleTime(df)
+
+    ####################
+    # Outlier handling #
+    # drops any rows with a tripDuration larger than 3 times interquantile range
+    OutlierHandling(df)
+
+    df.to_pickle("Data/DTU_DriveNow_Cleaned_Data")
+
 
 #########################
 # Plotting rides on map #
-plot_map(df)
+# plot_map(df)
+plot_trip_per_day(df)
+plot_trip_per_weekday(df)
+print(statistics_table(df))
